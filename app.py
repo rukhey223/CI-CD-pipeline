a06b7b8e9d0ccf80fd64405b298a736fea1c01c0
Mahrukh Bushra <mahrukhbushra159@gmail.com>
2026-05-02 02:18:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "CI/CD Pipeline is Live!"
+    return "Automation is 100% Complete!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
